426bdd2eae152f5eb6d4ba61cddb39ba630173dd
SamyKhelfa <thebboysams@gmail.com>
2020-11-02 15:43:52 +0100

    update

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const empty = require('./eventbox')
+const { empty, withArgs } = require('./eventbox')
+const { duplicate, transform } = require('./streambox')
 
+console.log('----- Eventbox')
+// eventbox
 empty()
+withArgs([
+  'Luffy',
+  'Zoro',
+  'Usopp',
+  'Robin',
+  'Nami',
+  'Sanji',
+  'Ch0pper',
+])
--- streambox.js
+++ streambox.js
@@ -1,2 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+const { Transform } = require 
+
+const {name, ext} = path.parse(filename) 
+
 const readStream = fs.createReadStream(filename)
 const writeStream = fs.createWriteStream('${name}.copy${ext}')
